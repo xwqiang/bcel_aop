5d53d928f4e4c2bcd4588c4a01c4c3798477096e
xwqiang <642143714@qq.com>
2015-03-24 17:24:42 +0800

    'init'

---
+++ Sample.java
@@ -0,0 +1,11 @@
+
+public class Sample {
+
+	public static String helloWorld(){
+		System.out.println("hello world");
+		return null;
+	}
+	public static void main(String[] args) {
+		helloWorld();
+	}
+}
